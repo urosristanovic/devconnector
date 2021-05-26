0e47b78e1551338936ecad2d4789c6bdc255656f
Uros Ristanovic <urosristanovic@gmail.com>
2021-05-26 13:53:57 +0200

    Create profile component and action.

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,5 @@
-import { GET_PROFILE, PROFILE_ERROR } from '../actions/types';
+import axios from 'axios';
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from '../actions/types';
 
 const initialState = {
   profile: null,
@@ -22,6 +23,13 @@ export default function (state = initialState, action) {
       return {
         ...state,
         error: payload,
+        loading: false,
+      };
+    case CLEAR_PROFILE:
+      return {
+        ...state,
+        profile: null,
+        repos: [],
         loading: false,
       };
     default:
