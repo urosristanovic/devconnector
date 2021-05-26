ec2cab2ce9c06f8784683c40835bb93cbb6c1c6d
Uros Ristanovic <urosristanovic@gmail.com>
2021-05-26 19:44:09 +0200

    Finished profile actions, displayed profiles and fixed little bugs.

--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -124,4 +124,4 @@ AddEducation.propTypes = {
   addExperience: PropTypes.func.isRequired,
 };
 
-export default connect(null, { addEducation })(AddEducation);
+export default connect(null, { addEducation })(withRouter(AddEducation));
